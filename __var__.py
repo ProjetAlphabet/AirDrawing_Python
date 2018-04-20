f20b5bbfe9f08a4b1fa0cd0e7aee7ba595317e0f
Cécile Becquié <cecilebecquie@MacBook-de-Cecile.local>
2018-04-20 12:31:16 +0200

    Modification des variables
    Ajout de nouvelles variables et réorganisation de la structure du projet

--- __var__.py
+++ __var__.py
@@ -5,9 +5,16 @@ Created on Wed Apr 18 17:02:43 2018
 @author: blook
 """
 
+# Définition du nom des modèles
+# /!\ SAISIR LES NOMS ET LES MODELES CORRESPONDANT AU MEME INDICE /!\ #
+models_aplh_name = []
+models_nums_name = []
+models_shap_name = ["carré", "cercle", "losange", "triangle"]
+
+# Definition du chemin des modèles
 models_alph = []
 models_nums = []
-models_shap = []
+models_shap = ["./models/shapes/carre_model.png", "./models/shapes/cercle_model.png", "./models/shapes/losange_model.png", "./models/shapes/triangle_model.png"]
 
 # Variables globales
 draw = "./__temp__/draw.png"
