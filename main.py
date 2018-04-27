4b9b4bcef64548c3575695df4da292311e51ecfd
Marie-Lea <34789504+Marie-Lea@users.noreply.github.com>
2018-04-27 18:10:01 +0200

    .modif interface

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from tkinter import *
 import tkinter.messagebox
 import tkinter.filedialog
 from tkinter import colorchooser
-import os
-import __var__ as glb
-
-#A FAIRE REUSSIR A EXECUTER LA SECONDE FENETRE CONTENANT LE TRACE ET LE RESULTAT
 
 def repondre():
  affichage['text'] = reponse.get()  # lecture du contenu du widget "reponse"
@@ -24,12 +20,15 @@ def Nom():
 #    affichage.pack()
     print(reponse)
 
+def Fermer():
+    Canevas.delete(ALL)
+    Mafenetre.title("Air drawing - mini projet")
 
 def Astuce():
     tkinter.messagebox.showinfo("Astuce","Maintenir une position stable")
 
 def Apropos():
-    tkinter.messagebox.showinfo("A propos","Python & Tkinter\n(C) Guillaume Obin\n Cécile Becquie\n Emilie Vintrou\n Marie-Léa Hupin")
+    tkinter.messagebox.showinfo("A propos","Python & Tkinter\n(C) Guillaume Obin\n Cécile Becquie\n Emilie Vintroe\n Marie-Léa Hupin")
 
 def couleura():
     Frame0['bg'] = "blue"
@@ -37,9 +36,6 @@ def couleura():
     Frame2['bg'] = "blue"
     Frame3['bg'] = "blue"    
     Mafenetre['bg'] = "blue"
-    Frame4['bg'] = "blue"
-    Frame42['bg'] = "blue"
-    Frame43['bg'] = "blue"
 
 def couleurb():
     Frame0['bg'] = "bisque"    
@@ -47,9 +43,6 @@ def couleurb():
     Frame2['bg'] = "bisque"
     Frame3['bg'] = "bisque" 
     Mafenetre['bg'] = "bisque"
-    Frame4['bg'] = "bisque"
-    Frame42['bg'] = "bisque"
-    Frame43['bg'] = "bisque"
 
 def couleurfond():
     color = colorchooser.askcolor()
@@ -61,19 +54,16 @@ def couleurfond():
     Frame1['bg'] = couleur2
     Frame2['bg'] = couleur2
     Frame3['bg'] = couleur2
+    Titre['bg'] = couleur2
+    deco['bg'] = couleur2
     Frame4['bg'] = couleur2
     Frame42['bg'] = couleur2
     Frame43['bg'] = couleur2
-    diff1["text"] = "la couleur de la spatule!"
-    diff1['bg'] = couleur2
-    quest['bg'] = couleur2
-    Titre1['bg'] = couleur2
-    deco1['bg'] = couleur2
-    Nom1['bg'] = couleur2
 
 def épaisseur():
     a = int(valeur.get())
-    diff["text"] = a, "pixels"   
+    diff["text"] = a
+    
     
 """
 def Ouvrir():
@@ -85,7 +75,7 @@ def Ouvrir():
 
 
 #___Fentre principale, Main window
-Mafenetre = Toplevel()
+Mafenetre = Tk()
 Mafenetre.title("Air drawing - mini projet")
 Mafenetre.geometry("900x900")
 Mafenetre.resizable(width=False,height=False)
@@ -110,15 +100,14 @@ Mafenetre.config(menu=menubar)
 #___Insertion du titre
 
 # Image titre
-photo = PhotoImage(file="./res/UI/titre.png")
+photo = PhotoImage(file="titre.png")
 photo_taille=photo.subsample(2,2)
 
 # création d'un widget Frame appellé titre dans Mafenetre
 Titre = Frame(Mafenetre,borderwidth=0,relief=GROOVE, bg="black")
 Titre.pack(padx=0,pady=0)
 
-Titre1=Label(Titre, image=photo_taille, bg="black")
-Titre1.pack()
+Label(Titre, image=photo_taille, bg="black").pack()
 
 """# Création d'une zone graphique (widget canvas)
 Largeur = 800
@@ -131,20 +120,19 @@ Canevas.pack(anchor=CENTER)"""
 #___Insertion déco
 
 # Image deco
-photodeco = PhotoImage(file="./res/UI/2.png")
+photodeco = PhotoImage(file="2.png")
 photodeco_taille=photodeco.subsample(2,2)
 
 deco = Frame(Mafenetre,borderwidth=0,relief=GROOVE, bg="black")
 deco.pack(padx=0,pady=0)
 
-deco1=Label(deco, image=photodeco_taille, bg="black")
-deco1.pack(side=LEFT)
+Label(deco, image=photodeco_taille, bg="black").pack(side=LEFT)
 
 #___Nom d'utilisateur
 
 # Création d'une zone graphique (widget canvas)
 Nom1 = Canvas(deco,width = 71, height =75, bg="black", bd=0)
-U = PhotoImage(file="./res/UI/utilisateur.png")
+U = PhotoImage(file="utilisateur.png")
 U_2 = U.subsample(2,2)
 Nom1.create_image(0,0,anchor=NW, image=U_2)
 print("jusqu'ici Nom1")
@@ -185,44 +173,40 @@ Frame43.pack(side=RIGHT,padx=5,pady=10)
 
 # création d'un widget Label et d'un widget Button dans un widget Frame
 Label(Frame1,text="Choix de la représentation:\n une lettre", bg="black", fg="white").pack(padx=1,pady=1)
-A =PhotoImage(file="./res/UI/a.png")
+A =PhotoImage(file="a.png")
 AA = A.subsample(2,2)
 Button(Frame1,image=AA,bg='black',command=couleura, cursor="star").pack(padx=1,pady=1)
 
 Label(Frame2,text="Choix de la représentation:\n un chiffre", bg="black", fg="white").pack(padx=1,pady=1)
-B =PhotoImage(file="./res/UI/1.png")
+B =PhotoImage(file="1.png")
 BB = B.subsample(2,2)
 Button(Frame2,image=BB,bg='black',command=couleurb, cursor="star").pack(padx=1,pady=1)
 
 Label(Frame3,text="Choix de la représentation:\n une forme", bg="black", fg="white").pack(padx=1,pady=1)
-C =PhotoImage(file="./res/UI/triangle.png")
+C =PhotoImage(file="triangle.png")
 CC = C.subsample(2,2)
 Button(Frame3,image=CC,bg='black',command=Frame0.destroy, cursor="star").pack(padx=1,pady=1)
 
 #___Epaisseur du trait
 Label(Frame4,text="Choix de l'épaisseur du trait", bg="black", fg="white").pack(padx=1,pady=10)
-diff = Label(Frame4,text="10 pixels", bg="green", fg="white")
+diff = Label(Frame4, bg="green", fg="white")
 diff.pack(pady=5)
 valeur = DoubleVar()
-scale1 = Scale(Frame4,from_ = 10, to = 20, variable = valeur ,orient = HORIZONTAL, resolution = 1, cursor="pencil",bd=0, bg="black", fg="white")
+scale1 = Scale(Frame4,from_ = 1, to = 10, variable = valeur ,orient = HORIZONTAL, resolution = 1, cursor="pencil",bd=0, bg="black", fg="white")
 scale1.pack(pady=10, padx=23)
 Button(Frame4, text="C'est partit!", command = épaisseur).pack(padx=10,pady=5)
 
 #___couleur du tracé 
 
-quest=Label(Frame43,text="De quelle couleur\nsera votre tracé?",bg="black", fg="white")
-quest.pack(padx=20,pady=5)
+Label(Frame43,text="De quelle couleur\nsera votre tracé?",bg="black", fg="white").pack(padx=20,pady=5)
 Button(Frame43,text="Définir ma couleur", command=couleurfond, cursor="pencil").pack(padx=10,pady=5)
-Label(Frame43,text="La couleur choisie est",bg="black", fg="white").pack(padx=20,pady=5)
-diff1 = Label(Frame43, bg="red", fg="white")
-diff1.pack(pady=5)
 
 #___Résultat
 
-Label(Frame42,text="/!\Attention/!\ \n Le tracé débute dès que la couleur sélectionné \n est détectée, retournez la spatule \nlorsque vous êtes prêts.",bg="black", fg="white").pack(padx=5,pady=10)
+Label(Frame42,text="/!\Attention/!\ \n Le tracé débute dès que la couleur sélectionné est détectée, retournez la spatule lorsque vous êtes prêts.",bg="black", fg="white").pack(padx=5,pady=10)
 """Fin = Canvas(Frame42,width = 50, height =50, bg="black", bd=0)
 Fin.pack(padx=3,pady=3)"""
-Button(Frame42,text="réaliser un nouveau tracé", command=Mafenetre.destroy, cursor="pencil").pack(padx=10,pady=10)
+Button(Frame42,text="réaliser un nouveau tracé", command=Fermer, cursor="pencil").pack(padx=10,pady=10)
 
 
 Mafenetre.mainloop()
